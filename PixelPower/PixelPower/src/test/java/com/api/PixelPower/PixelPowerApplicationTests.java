2cb67c239dec6f33a6f8282cae766019d1264670
MahaDaiki <142234887+MahaDaiki@users.noreply.github.com>
2025-01-08 14:08:21 +0100

    Add files via upload
    Creation  du projet springboot

---
+++ PixelPower/PixelPower/src/test/java/com/api/PixelPower/PixelPowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.PixelPower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelPowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
