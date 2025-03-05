e1df52724d8a85e656fa229b3271697b1f64b9a6
MahaDaiki <mahadaiki2@gmail.com>
2025-03-05 15:45:08 +0000

    pp-52  navbar

--- PixelPower/PixelPowerFront/src/app/app.module.ts
+++ PixelPower/PixelPowerFront/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { NavbarComponent } from './component/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
