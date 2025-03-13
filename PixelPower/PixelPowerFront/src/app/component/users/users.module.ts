f9bb9f678b9f54568f22733e3b2f2c91b5e795ad
MahaDaiki <mahadaiki2@gmail.com>
2025-03-13 13:56:22 +0000

    PP-24 Config crud  add/display

--- PixelPower/PixelPowerFront/src/app/component/users/users.module.ts
+++ PixelPower/PixelPowerFront/src/app/component/users/users.module.ts
@@ -7,6 +7,8 @@ import {UsersService} from '../../service/users.service';
 import {userReducer} from '../../store/users/users.reducer';
 import {StoreModule} from '@ngrx/store';
 import {ReactiveFormsModule} from '@angular/forms';
+import {GamesModule} from "../games/games.module";
+import {ConfigurationsModule} from "../configurations/configurations.module";
 
 
 @NgModule({
@@ -15,6 +17,7 @@ import {ReactiveFormsModule} from '@angular/forms';
   ],
   imports: [
     CommonModule,
+    ConfigurationsModule,
     UsersRoutingModule,
     ReactiveFormsModule,
     StoreModule.forFeature('user', userReducer),
