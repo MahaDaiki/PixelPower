da692b5b1382bf76badea31f8ebecfb49b04c759
MahaDaiki <mahadaiki2@gmail.com>
2025-03-21 10:27:09 +0000

    PP-54 DOCKER

--- PixelPower/PixelPowerFront/src/app/component/shared/navbar/navbar.component.ts
+++ PixelPower/PixelPowerFront/src/app/component/shared/navbar/navbar.component.ts
@@ -7,45 +7,7 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   selector: 'app-navbar',
   standalone: false,
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
-  animations: [
-    trigger("fadeIn", [
-      state(
-        "void",
-        style({
-          opacity: 0,
-        }),
-      ),
-      transition(":enter", [
-        animate(
-          "0.5s ease-out",
-          style({
-            opacity: 1,
-          }),
-        ),
-      ]),
-    ]),
-    trigger("glitchText", [
-      state(
-        "normal",
-        style({
-          transform: "translateX(0)",
-        }),
-      ),
-      state(
-        "glitch",
-        style({
-          transform: "translateX(0)",
-        }),
-      ),
-      transition("normal <=> glitch", [
-        animate("0.1s", style({ transform: "translateX(10px)" })),
-        animate("0.1s", style({ transform: "translateX(-10px)" })),
-        animate("0.1s", style({ transform: "translateX(5px)" })),
-        animate("0.1s", style({ transform: "translateX(0)" })),
-      ]),
-    ]),
-  ],
+  styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   isMenuOpen = false;
